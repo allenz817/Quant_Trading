e42ffe7a63f399370e2c9a0d59bffb2b6b82dcad
allenz817 <allenjzhao@gmail.com>
2025-02-12 22:12:20 +0800

    Amended

--- strategy_test.py
+++ strategy_test.py
@@ -3,7 +3,9 @@ from backtesting.lib import crossover
 from backtesting.test import SMA, GOOG
 import yfinance as yf
 import pandas as pd
+import talib as ta
 
+#IMPLEMENTATION
 class SmaCross(Strategy):
     n1 = 10
     n2 = 20
@@ -19,13 +21,31 @@ class SmaCross(Strategy):
         elif crossover(self.sma2, self.sma1):
             self.sell()
 
-#get financial data from yfinance
+class RsiIndicator(Strategy):
+    rsi_period = 14
+    rsi_overbought = 70
+    rsi_oversold = 30
+    
+    def init(self):
+        close = self.data.Close
+        self.rsi = self.I(ta.RSI, close, self.rsi_period)
+    
+    def next(self):
+        if crossover(self.rsi, self.rsi_oversold):
+            self.buy()
+        elif crossover(self.rsi_overbought, self.rsi):
+            self.sell()
+
+
+
+# BACKTESTING
+# get financial data from yfinance
 stock = yf.download('GOOG', start='2020-01-01', end='2020-12-31')[
     ['Open', 'High', 'Low', 'Close', 'Volume']]
-#reshape multi-index columns
+# reshape multi-index columns
 stock.columns = stock.columns.droplevel(1) 
 
-bt = Backtest(stock, SmaCross,
+bt = Backtest(stock, RsiIndicator,
               cash=10000, commission=.002,
               exclusive_orders=True)
 
